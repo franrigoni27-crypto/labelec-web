18ebb2abf07b07e8ad9e2e3fa91a2624a8991f80
Fran <fran.rigoni.27@gmail.com>
2026-01-21 12:33:58 -0300

    cambio12

--- crearAdmin.js
+++ crearAdmin.js
@@ -9,8 +9,8 @@ const crearUsuario = async () => {
     try {
         // --- CONFIGURA AQUÍ TU USUARIO Y CONTRASEÑA ---
         const nuevoAdmin = new User({
-            username: 'admin',        // Puedes cambiar 'admin' por tu nombre
-            password: 'password123'   // ¡Pon una contraseña segura aquí!
+            username: 'labelec',        // Puedes cambiar 'admin' por tu nombre
+            password: 'lblc1482'   // ¡Pon una contraseña segura aquí!
         });
 
         // Al guardar, el modelo User.js encriptará la contraseña automáticamente
